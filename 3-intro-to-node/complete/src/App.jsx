b0128c2131685cd25ad881f2a80a88a9ab968f5d
KindaOK <27974340+KindaOK@users.noreply.github.com>
2022-10-06 18:59:01 -0400

    Add spinner from `react-spinners`
    Demo for using another package

--- 3-intro-to-node/complete/src/App.jsx
+++ 3-intro-to-node/complete/src/App.jsx
@@ -3,11 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import DynamicButtons from "./DynamicButtons";
 import EditableText from "./EditableText";
+import { ClimbingBoxLoader } from "react-spinners";
 
 function App() {
   return (
     <div className="App">
       <img className={"App-logo"} src={logo} />
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <ClimbingBoxLoader />
+      </div>
       <EditableText />
       <DynamicButtons />
     </div>
